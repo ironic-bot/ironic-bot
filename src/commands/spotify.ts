c253601dd22d1b7d1421b612f8a328df5ced9db4
SapphireDevI <128000238+SapphireDevI@users.noreply.github.com>
2023-07-10 22:34:24 -0300

    fix build

--- src/commands/spotify.ts
+++ src/commands/spotify.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { ICommand } from '../interfaces/ICommand.js';
 import { ChatInputCommandInteraction, Client, SlashCommandBuilder } from 'discord.js';
 import got from 'got';
+import { AccessToken, Paging, Track } from "spotify-types";
 import fs from 'fs';
 import * as bing from '../bing.js';
 import { db } from '../glob.js';
@@ -23,30 +24,20 @@ export default class implements ICommand {
 
     async run(_client: Client, interaction: ChatInputCommandInteraction): Promise<void> {
         if (!fs.existsSync(process.env.COOKIE_FILE ?? '')) {
-			await interaction.reply('Bing support not enabled!');
-			return;
-		}
+            await interaction.reply('Bing support not enabled!');
+            return;
+        }
 
         if (interaction.options.getSubcommand() === 'authorize') {
-            const spotifyData = db
-                .prepare(
-                    'SELECT * FROM spotify WHERE user_id = ?',
-                )
-                .get(interaction.user.id) as { user_id: string; access_token: string; refresh_token: string };
+            const usedState = global.spotifyStates.find(x => x.userId === interaction.user.id);
 
-            if (!spotifyData) {
-                const usedState = global.spotifyStates.find(x => x.userId === interaction.user.id);
+            if (!usedState) {
+                const uuid = uuidv4();
+                global.spotifyStates.push({ userId: interaction.user.id, state: uuid });
 
-                if (!usedState) {
-                    const uuid = uuidv4();
-                    global.spotifyStates.push({ userId: interaction.user.id, state: uuid });
-
-                    await interaction.reply({ content: `Use this link to authorize Ionic to access your Spotify account. This link is only for you, so don't share it. ${process.env.SPOTIFY_AUTHORIZE_URI}?state=${uuid}`, ephemeral: true });
-                } else {
-                    await interaction.reply({ content: `Use this link to retry to authorize Ionic to access your Spotify account. This link is only for you, so don't share it. ${process.env.SPOTIFY_AUTHORIZE_URI}?state=${usedState.state}`, ephemeral: true });
-                }
+                await interaction.reply({ content: `Use this link to authorize Ionic to access your Spotify account. This link is only for you, so don't share it. ${process.env.SPOTIFY_AUTHORIZE_URI}?state=${uuid}`, ephemeral: true });
             } else {
-                await interaction.reply({ content: 'Ionic is already authorized to access your Spotify account!', ephemeral: true });
+                await interaction.reply({ content: `Use this link to retry to authorize Ionic to access your Spotify account. This link is only for you, so don't share it. ${process.env.SPOTIFY_AUTHORIZE_URI}?state=${usedState.state}`, ephemeral: true });
             }
         } else if (interaction.options.getSubcommand() === 'recommend') {
             await interaction.deferReply();
@@ -57,39 +48,40 @@ export default class implements ICommand {
                 .get(interaction.user.id) as { user_id: string; access_token: string; refresh_token: string };
 
             if (spotifyData) {
-
-
-                new Promise(async resolve => {
-                    resolve(await got.get('https://api.spotify.com/v1/me/top/tracks', {
-                        headers: {
-                            'Authorization': 'Bearer ' + spotifyData.access_token
-                        }
-                    }).json().catch(async () => {
-                        const jsonRefresh = await got.post('https://accounts.spotify.com/api/token', {
-                            form: {
-                                grant_type: 'refresh_token',
-                                refresh_token: spotifyData.refresh_token
-                            },
-                            headers: {
-                                'Authorization': 'Basic ' + Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
-                            }
-                        }).json();
-    
-                        db.prepare(
-                            'UPDATE spotify SET access_token = ? WHERE user_id = ?',
-                        ).run(
-                            (jsonRefresh as any).access_token,
-                            interaction.user.id
-                        );
-    
+                new Promise((resolve, reject) => {
+                    (async () => {
                         resolve(await got.get('https://api.spotify.com/v1/me/top/tracks', {
                             headers: {
-                                'Authorization': 'Bearer ' + (jsonRefresh as any).access_token
+                                'Authorization': 'Bearer ' + spotifyData.access_token
                             }
-                        }).json());
-                    }));
-                }).then(async json => {
-                    const stuffy = (json as any).items.map(x => x.name + ' by ' + x.artists.map(x => x.name).join(', '));
+                        }).json<Paging<Track>>().catch(() => {
+                            got.post('https://accounts.spotify.com/api/token', {
+                                form: {
+                                    grant_type: 'refresh_token',
+                                    refresh_token: spotifyData.refresh_token
+                                },
+                                headers: {
+                                    'Authorization': 'Basic ' + Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+                                }
+                            }).json<AccessToken>().then(async jsonRefresh => {
+
+                                db.prepare(
+                                    'UPDATE spotify SET access_token = ? WHERE user_id = ?',
+                                ).run(
+                                    jsonRefresh.access_token,
+                                    interaction.user.id
+                                );
+
+                                resolve(await got.get('https://api.spotify.com/v1/me/top/tracks', {
+                                    headers: {
+                                        'Authorization': 'Bearer ' + jsonRefresh.access_token
+                                    }
+                                }).json<Paging<Track>>());
+                            }).catch(() => reject());
+                        }));
+                    })();
+                }).then(async (json: Paging<Track>) => {
+                    const stuffy = json.items.map(x => x.name + ' by ' + x.artists.map(x => x.name).join(', '));
 
                     const bingEngine = await bing.initialize();
 
@@ -98,6 +90,8 @@ export default class implements ICommand {
                     const finalJson = JSON.parse(bingReply.split("```").find(x => x.startsWith('json')).slice(4));
 
                     await interaction.editReply(`Here are ten song recommendations by **Bing Chat**:\n${finalJson.songs.map(x => `**${x.title}** by **${x.artist}**`).join('\n')}`);
+                }).catch(async () => {
+                    await interaction.editReply('You must authorize Ionic to access your Spotify account!');
                 });
             } else {
                 await interaction.editReply('You must authorize Ionic to access your Spotify account!');
